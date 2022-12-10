38605bf0c5785c629b797d2e1139bd1c493c1d93
juwamardood <juwamardood041@gmail.com>
2022-12-11 02:59:59 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Portofolio | I Gede Juwa Mardood</title>
+  </head>
+  <audio hidden autoplay loop>
+    <source src="music/kembangsakura.mp3" type="audio/mp3">
+</audio>
+<nav class="navbar bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-primary" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!-- Jumbotron -->
+    <section id="jumbotron"
+        class ="jumbotron text-center mb-0">
+        <img src="img/ktm.jpeg" alt="Mardood" width="120" height="20" class="rounded-circle img-thumbnail"/>
+        <h3 class="display-30">I Gede Juwa Mardood</h3>
+        <pc class="lead">Mahasiswa</p>
+  <div class="spinner-border text-primary" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-success" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-danger" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-warning" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-info" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-light" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+  <div class="spinner-border text-dark" role="status">
+  <span class="visually-hidden">hi</span>
+  </div>
+        
+    </section>
+<!-- Akhir jumbotron -->
+<!-- sosmed -->
+<section id="sosmed"> 
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+      </div>
+    </div>
+    <div class="row text-center mb-5">
+      <div class="col">
+      <a href="https://www.facebook.com/Juwamardott">
+          <img src="img/facebook.png" widht="50 " height="50">
+          <p class>Facebook</p>
+      </a>
+      </div>
+      <div class="col">
+      <a href="https://wa.me/6285847968314">
+          <img src="img/whatsapp.png" widht="50" height="50">
+          <p class>Whatsapp</p>
+      </a>
+      </div>
+      <div class="col">
+      <a href="https://www.instagram.com/juwamardott">
+          <img src="img/instagram.png" widht="50" height="50">
+          <p class>Instagram</p>
+      </a>  
+      </div>
+      <div class="col">
+      <a href="https://www.twitter.com/juwamardott">
+          <img src="img/twitter.png" widht="50" height="50">
+          <p class>Twitter</p>
+      </a>  
+    </div>
+  </div>
+  <!-- akhir sosmed -->
+
+<!-- placeholder -->
+<!-- akhir placheholder -->
+<!-- about --
+  <section id="About">
+    <div class="container">
+      <div class="row text-center mb-1">
+        <div class="col">
+          <h3>About me</h3>
+        </div>
+      </div>
+      <div class="col text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ipsum quo enim, voluptatum accusamus assumenda aperiam maxime hic beatae ipsa voluptatibus harum est quidem eius esse labore pariatur quam recusandae repellat veniam dolorum tempora quas maiores aliquid. Quod ullam architecto explicabo laudantium fuga error facere nihil quibusdam at voluptates? Nihil?</p>
+      </div>
+    </div>
+  </section>
+</section>
+
+Project -->
+<section id="Project">
+  <div class="container">
+    <div class="row text-center mb-1">
+      <div class="col">
+        <p>My story</p>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-4 mb-3">
+            <div class="card">
+          <img src="img/pr1.jpg" class="card-img-top" alt="project1">
+          <div class="card-body">
+        <p class="card-text">Laptop 1</p>
+      </div>
+      </div>
+
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+          <img src="img/pr2.jpg" class="card-img-top" alt="project2">
+          <div class="card-body">
+        <p class="card-text">Laptop 2</p>
+      </div>
+      </div>
+
+        </div>  
+        <div class="col-md-4 mb-3">
+            <div class="card">
+          <img src="img/pr3.jpg" class="card-img-top" alt="project3">
+          <div class="card-body">
+        <p class="card-text">Laptop 3</p>
+      </div>
+      </div>
+
+        </div>
+    </div>
+  </div>
+</section>
+<!--  akhir Project -->
+
+<!-- Con -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
